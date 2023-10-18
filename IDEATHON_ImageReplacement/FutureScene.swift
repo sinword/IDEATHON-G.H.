a6caeb0bca3b20a439f1d22fd92c51aff8d977ed
Yi0327 <samwang9199@gmail.com>
2023-10-18 23:13:08 +0800

    init commit

---
+++ IDEATHON_ImageReplacement/FutureScene.swift
@@ -0,0 +1,13 @@
+//
+//  scene.swift
+//  IDEATHON_ImageReplacement
+//
+//  Created by 新翌王 on 2023/10/18.
+//
+
+import Foundation
+
+struct Scene: Decodable {
+    let name: String
+    let intro: String
+}
