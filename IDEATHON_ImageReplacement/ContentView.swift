50380d8e68acbde8c517fba2c172a2e69a56bbda
Yi0327 <samwang9199@gmail.com>
2023-10-23 03:46:14 +0800

    top UI complete & frame problem

---
+++ IDEATHON_ImageReplacement/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  IDEATHON_ImageReplacement
+//
+//  Created by 新翌王 on 2023/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
