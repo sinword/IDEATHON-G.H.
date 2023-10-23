6c47c9c2eca720f6aa755af46dad82bfc8796003
Yi0327 <samwang9199@gmail.com>
2023-10-23 22:44:28 +0800

    Translucent mask in progress

--- IDEATHON_ImageReplacement/ContentView.swift
+++ IDEATHON_ImageReplacement/ContentView.swift
@@ -26,46 +26,56 @@ struct ContentView: View {
         // Overlay ARview and UI
         // ARview is defined in ViewController.swift
         // Set ARview as background
-        // ZStack follows the size of 834.0 x 1194.0
         ZStack {
-           ARUIView()
-           ZStack {
-               Image("TopVector1")
-               VStack(alignment: .leading) {
-                   HStack {
-                       Image("VRHeadset")
-                           .frame(width: 35.12657, height: 17.15551)
-                           .padding()
-                       Text("客製化你的城市")
-                           .font(.custom("NotoSansCJKTC", size: 17.68))
-                           .kerning(9.6356)
-                           .foregroundColor(.white)
-                   }
-                   .position(x: 130, y: 25)
-                   
-                   HStack {
-                       Text("Customize your city")
-                         .font(Font.custom("Syncopate", size: 15.5272))
-                         .foregroundColor(.white)
-                         .position(x: 130, y: 20)
-                       Spacer()
-                       Text("G.H.                                   頭盔畫面測試 Beta v2.5.3")
-                         .font(
-                           Font.custom("Syncopate", size: 17.68)
-                               .weight(.bold)
-                         )
-                         .foregroundColor(.black)
-                         .padding()
-                   }
-               }
-               .zIndex(2)
-               Image("TopVector2")
-                   .frame(width: 83.41927, height: 16.79277)
-                   .offset(x: -230, y: 3)
-           }
-           .frame(width: UIScreen.main.bounds.width, height: 89)
-           .position(x: UIScreen.main.bounds.width / 2, y: 20)
-       }
+            // Give ARUIView a translucent mask
+            ARUIView()
+                .mask(
+                    Rectangle()
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                        .foregroundColor(.white)
+                        .opacity(0.4)
+                )
+            
+            ZStack {
+                Image("TopVector1")
+                VStack {
+                    HStack {
+                        Image("VRHeadset")
+                        .frame(width: 35.12657, height: 17.15551)
+                        .padding()
+                    Text("客製化你的城市")
+                        .font(.custom("NotoSansCJKTC", size: 17.68))
+                        .kerning(9.6356)
+                        .foregroundColor(.white)
+                    }
+                    .position(CGPoint(x: 140, y: 32))
+
+                    HStack (alignment: .center) {
+                        Text("Customize your city")
+                            .font(Font.custom("Syncopate", size: 15.5272))
+                            .foregroundColor(.white)
+                            .padding(20)
+
+                        Spacer()
+                        Text("G.H")
+                            .font(Font.custom("Syncopate", size: 17.68).weight(.bold))
+                            .foregroundColor(.black)
+                        Spacer()
+
+                        Text("頭盔畫面測試 Beta v2.5.3")
+                            .font(Font.custom("Syncopate", size: 17.68).weight(.bold))
+                            .foregroundColor(.black)
+                            .padding()
+                    }   
+                }
+                .zIndex(2)
+                Image("TopVector2")
+                    .frame(width: 83.41927, height: 16.79277)
+                    .offset(x: -230, y: 3)
+            }
+            .frame(width: UIScreen.main.bounds.width, height: 89)
+            .position(x: UIScreen.main.bounds.width / 2, y: 20)
+        }
     }
 }
 
