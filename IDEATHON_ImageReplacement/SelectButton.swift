8895391e9634614be6bf9bab0d5d095a83f69c46
Yi0327 <samwang9199@gmail.com>
2023-10-25 19:06:11 +0800

    Just in case

---
+++ IDEATHON_ImageReplacement/SelectButton.swift
@@ -0,0 +1,18 @@
+//
+//  SelectButton.swift
+//  IDEATHON_ImageReplacement
+//
+//  Created by 新翌王 on 2023/10/25.
+//
+
+import SwiftUI
+
+struct SelectButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectButton()
+}
